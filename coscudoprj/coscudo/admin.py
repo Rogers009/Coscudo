12f513cea7fe298bf9f9c4186cb8dbf069d263dc
Rogers009 <86000347+Rogers009@users.noreply.github.com>
2022-01-12 17:11:29 +0530

    Add files via upload

---
+++ coscudoprj/coscudo/admin.py
@@ -0,0 +1,6 @@
+from coscudo.models import login, regist
+from django.contrib import admin
+
+# Register your models here.
+admin.site.register(regist)
+admin.site.register(login)
---
+++ coscudoprj/coscudo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CoscudoConfig(AppConfig):
+    name = 'coscudo'
