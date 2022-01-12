0dec5eeea72ba7c983bbcb0f684f723bcdb6ca29
Rogers009 <86000347+Rogers009@users.noreply.github.com>
2022-01-12 17:14:39 +0530

    Delete coscudoprj directory

--- coscudoprj/coscudo/admin.py
+++
@@ -1,6 +0,0 @@
-from coscudo.models import login, regist
-from django.contrib import admin
-
-# Register your models here.
-admin.site.register(regist)
-admin.site.register(login)
--- coscudoprj/coscudo/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoscudoConfig(AppConfig):
-    name = 'coscudo'
